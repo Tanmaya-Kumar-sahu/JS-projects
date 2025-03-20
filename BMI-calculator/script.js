fd0499251f555938ae6fa8ce268af9a63c433a94
Tanmaya-Kumar-sahu <tanmayakumarsahu456@gmail.com>
2025-03-20 12:29:25 +0000

    BMI calculator

---
+++ BMI-calculator/script.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('form')
+//this usecase is giving you empty value
+//parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit',function(e){
+
+    e.preventDefault()
+
+   const height = parseInt(document.querySelector('#height').value);
+   const weight = parseInt(document.querySelector('#weight').value);
+   const result= parseInt(document.querySelector('#results'));
+
+    if (height==='' || height< 0 || isNaN(height)) {
+       result.innerHTML = `please enter a valid value ${height}` ;
+    }else if (weight==='' || height< 0 || isNaN(weight)) {
+       result.innerHTML = `please enter a valid value ${weight}` ;
+    }else{
+      const BMI =  (weight/((height*height)/10000)).toFixed(2)
+      //show the result
+      result.innerHTML = `<span>${BMI}</span>`
+    }
+})
